4683eb5590d82fc82de04d7232ba96e2b2dce8e9
gs6029907@gmail.com <gs6029907@gmail.com>
2025-10-21 11:03:59 +0530

    customer

--- app/main/customer/page.jsx
+++ app/main/customer/page.jsx
@@ -1,43 +1,66 @@
 'use client'
 import fetcher from "@/app/lib/fetcher";
-import { Skeleton, Table } from "antd";
-import useSWR from "swr";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Button, Skeleton, Table } from "antd";
+import useSWR, { mutate } from "swr";
+import axios from "axios";
 
 const CustomersPage = () => {
-   const{data , error , isLoading} =  useSWR('/customer' , fetcher)
-   console.log(data , error)
+    const { data, error, isLoading } = useSWR('/customer', fetcher)
+    console.log(data, error)
 
-   if(isLoading)
-    return <Skeleton/>
+    if (isLoading)
+        return <Skeleton />
 
-   const column = [
+    const column = [
         {
-            key : 'fullname',
-            title : 'Fullname',
-            dataIndex : 'fullname'
+            key: 'fullname',
+            title: 'Fullname',
+            dataIndex: 'fullname'
         },
         {
-            key : 'email',
-            title : 'Email',
-            dataIndex : 'email'
+            key: 'email',
+            title: 'Email',
+            dataIndex: 'email'
         },
         {
-            key : 'mobile',
-            title : 'Mobile',
-            dataIndex : 'mobile'
+            key: 'mobile',
+            title: 'Mobile',
+            dataIndex: 'mobile'
+        },
+        {
+            key: 'actions',
+            title: 'Actions',
+            render: (item) => {
+                return (
+                   <div className="flex gap-2">
+                     <div>
+                        <Button icon={<EditOutlined />} className="!text-violet-600 !border-violet-600 !border-2"/>
+                    </div>
+                    <div>
+                        <Button onClick={()=>deleteCustomers(item._id)} icon={<DeleteOutlined/>} className="!text-rose-600 !border-rose-600 !border-2"/>
+                    </div>
+                   </div>
+                )
+            }
         }
-   ]
-  return (
-    <>
-      <div>
-        <Table
-            columns={column}
-            dataSource={data}
-            rowKey="_id"
-        />
-      </div>
-    </>
-  );
+    ]
+
+    const deleteCustomers = async (id)=>{
+        await axios.delete(`/customer/${id}`)
+        mutate('/customer')
+    }
+    return (
+        <>
+            <div>
+                <Table
+                    columns={column}
+                    dataSource={data}
+                    rowKey="_id"
+                />
+            </div>
+        </>
+    );
 };
 
 export default CustomersPage;
