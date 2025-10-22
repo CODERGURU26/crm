5f2635aba9b17cef35227024e5908c0b52171707
gs6029907@gmail.com <gs6029907@gmail.com>
2025-10-22 10:50:54 +0530

    customer

--- app/main/customer/page.jsx
+++ app/main/customer/page.jsx
@@ -1,13 +1,21 @@
 'use client'
 import fetcher from "@/app/lib/fetcher";
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined, SearchOutlined, UserAddOutlined } from "@ant-design/icons";
 import { Button, Divider, Form, Input, Modal, Skeleton, Table } from "antd";
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 import useSWR, { mutate } from "swr";
 import axios from "axios";
+import { useState } from "react";
 
 const CustomersPage = () => {
     const { data, error, isLoading } = useSWR('/customer', fetcher)
     console.log(data, error)
+    const [open, setOpen] = useState(false)
+
+    const addCustomer = (values) => {
+
+    }
 
     if (isLoading)
         return <Skeleton />
@@ -33,62 +41,70 @@ const CustomersPage = () => {
             title: 'Actions',
             render: (item) => {
                 return (
-                   <div className="flex gap-2">
-                     <div>
-                        <Button icon={<EditOutlined />} className="!text-violet-600 !border-violet-600 !border-2"/>
+                    <div className="flex gap-2">
+                        <div>
+                            <Button icon={<EditOutlined />} className="!text-violet-600 !border-violet-600 !border-2" />
+                        </div>
+                        <div>
+                            <Button onClick={() => deleteCustomers(item._id)} icon={<DeleteOutlined />} className="!text-rose-600 !border-rose-600 !border-2" />
+                        </div>
                     </div>
-                    <div>
-                        <Button onClick={()=>deleteCustomers(item._id)} icon={<DeleteOutlined/>} className="!text-rose-600 !border-rose-600 !border-2"/>
-                    </div>
-                   </div>
                 )
             }
         }
     ]
 
-    const deleteCustomers = async (id)=>{
+    const deleteCustomers = async (id) => {
         await axios.delete(`/customer/${id}`)
         mutate('/customer')
     }
     return (
         <>
             <div>
+                <div className="flex gap-2 mb-5">
+                    <Input placeholder="Search Customer" prefix={<SearchOutlined className="!text-gray-500 mr-1"/>} />
+                    <Button size="large" type="primary" className="!bg-indigo-600" icon={<UserAddOutlined />}>Add Customer</Button>
+                </div>
+                <Divider/>
                 <Table
                     columns={column}
                     dataSource={data}
                     rowKey="_id"
                 />
-                <Modal open footer={null} title="Add Customers">
-                <Divider/>
+                <Modal open={open} footer={null} title="Add Customers">
+                    <Divider />
 
-                <Form layout="vertical">
-                    <Form.Item
-                        label="Customers Name:"  
-                        rules={[{required : true}]} 
-                        name="fullname"                 
-                    >
-                        <Input size="large" placeholder="Mr. Gururaj"/>
-                    </Form.Item>
+                    <Form layout="vertical" onFinish={addCustomer}>
+                        <Form.Item
+                            label="Customers Name:"
+                            rules={[{ required: true }]}
+                            name="fullname"
+                        >
+                            <Input size="large" placeholder="Mr. Gururaj" />
+                        </Form.Item>
 
-                      <Form.Item
-                        label="Email:"  
-                        rules={[{required : true}]} 
-                        name="email"                    
-                    >
-                        <Input size="large" placeholder="example@gmail.com"/>
-                    </Form.Item>
+                        <Form.Item
+                            label="Email:"
+                            rules={[{ required: true }]}
+                            name="email"
+                        >
+                            <Input size="large" placeholder="example@gmail.com" />
+                        </Form.Item>
 
-                      <Form.Item
-                        label="Mobile:"  
-                        rules={[{required : true}]} 
-                        name="mobile"                    
-                    >
-                        <Input size="large" placeholder="+91 9999999999"/>
-                    </Form.Item>
-                    <Form.Item>
-                        <Button size="large" htmlType="submit">Add Now</Button>
-                    </Form.Item>
-                </Form>
+                        <Form.Item
+                            label="Mobile:"
+                            rules={[{ required: true }]}
+                            name="mobile"
+                        >
+                            <PhoneInput
+                                country={'in'}
+                                inputClass="!w-full"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button icon={<UserAddOutlined />} type="primary" size="large" htmlType="submit">Add Now</Button>
+                        </Form.Item>
+                    </Form>
                 </Modal>
             </div>
         </>
